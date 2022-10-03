d0fea2eb191aa9ca204cad2cbc9e54b550f6ea19
Ab1603 <93122312+Ewasah@users.noreply.github.com>
2022-10-03 10:01:00 +0000

    buizen afgemaakt en bezig met de vogel

--- bird.js
+++ bird.js
@@ -1,12 +1,49 @@
-class Bird {
-    constructor() {
-        this.y = width/2;
-        this.x = 25;
+class Birb {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.vy = 0;
+        this.gravity = 0.2;
     }
-     
-    show() {
-        fill(255);
-        ellipse(this.x, this.y, 16, 16);
+
+    drawBirb() {
+        fill("red");
+        circle(this.x, this.y, 10)
+    }
+
+    if(this.y + this.h >= height) {
+    this.y = height - this.h;
+
+    if (this.velocity < 0) {
+        this.y += this.velocity;
     }
 }
-    
+
+        else {
+    this.velocity += this.acceleration;
+    this.y += this.velocity;
+}
+var birb;
+
+function setup() {
+    createCanvas(500, 400);
+
+    birb = new Birb(100, 200);
+
+}
+
+
+function draw() {
+    background(225);
+
+
+
+    birb.drawBirb();
+
+}
+
+function keyPressed() {
+    if (keyCode == 32) {
+        birb.vy -= 5;
+    }
+}
--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+var bird = []
+var pipes = []
+
 function setup() {
-    createCanvas(400, 600);
+    createCanvas(500, 400);
 }
 
 function draw() {
-    background("black")
+    background("#007dad");
+ 
+    if (frameCount % 50 == 0) {          
+        
+        let gapHeight = 150;        
+        let topPipeHeight = random(height - gapHeight);
+
+        pipes.push(new Pipe(550, 0, 75, topPipeHeight, "green"));
+        pipes.push(new Pipe(550, topPipeHeight + gapHeight, 75, 400, "green"));
+        //console.log(pipes);
+    }
+
+    pipes.forEach((pipe) => {
+        pipe.drawRect();
+    });
+    
+
 }
+
+function keyPressed() {
+    if (keyCode == 32) {
+        birb.vy -= 5;
+    }
+}
+
+
